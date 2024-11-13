26becf64949d57ed4230547a8d6a041b9ecfa918
EternalRival <eternalrival8@gmail.com>
2024-11-13 15:32:11 +0300

    feat: replace link widget with autoredirect

--- src/app/app.vue
+++ src/app/app.vue
@@ -3,7 +3,6 @@ import { useQuery } from '@tanstack/vue-query'
 import { computed, onBeforeMount, ref } from 'vue'
 import ContributorModal from '~/components/contributor-modal.vue'
 import CubeLoader from '~/components/cube-loader.vue'
-import RsschoolLink from '~/components/rsschool-link.vue'
 import SearchStudentInput from '~/components/search-student-input.vue'
 import StudentList from '~/components/student-list.vue'
 import type { Contributor } from '~/utils/get-gratitudes-summary'
@@ -11,7 +10,6 @@ import getGratitudesSummary from '~/utils/get-gratitudes-summary'
 
 const RSSCHOOL_URL = 'https://app.rs.school/'
 
-const isPathCorrect = ref<boolean>()
 const isModalOpen = ref<boolean>(false)
 const modalContent = ref<Contributor[]>([])
 const searchValue = ref<string>('')
@@ -19,7 +17,6 @@ const searchValue = ref<string>('')
 const { isPending, isError, data } = useQuery({
   queryKey: ['gratitudes'],
   queryFn: getGratitudesSummary,
-  enabled: isPathCorrect,
 })
 
 const filteredStudentList = computed(() => {
@@ -34,50 +31,42 @@ const handleOpenContributorsModal = (contributorList: Contributor[]) => {
 }
 
 onBeforeMount(async () => {
-  if (import.meta.env.MODE === 'development') {
-    isPathCorrect.value = true
-  } else {
-    const currentTabs = await chrome.tabs.query({ active: true, currentWindow: true })
+  if (import.meta.env.MODE === 'development') return
 
-    isPathCorrect.value = currentTabs.every((tab) => tab.url?.startsWith(RSSCHOOL_URL))
-  }
+  const currentTabs = await chrome.tabs.query({ active: true, currentWindow: true })
+  const isPathCorrect = currentTabs.every((tab) => tab.url?.startsWith(RSSCHOOL_URL))
+
+  if (isPathCorrect) return
+
+  await chrome.tabs.create({ url: RSSCHOOL_URL })
 })
 </script>
 
 <template>
-  <div
-    v-if="!isPathCorrect"
-    class="flex h-dvh items-center justify-center bg-white"
-  >
-    <RsschoolLink />
-  </div>
+  <header>
+    <SearchStudentInput v-model="searchValue" />
+  </header>
 
-  <div v-else>
-    <header>
-      <SearchStudentInput v-model="searchValue" />
-    </header>
+  <main>
+    <ContributorModal
+      :is-open="isModalOpen"
+      :contributor-list="modalContent"
+      @close="isModalOpen = false"
+    />
 
-    <main>
-      <ContributorModal
-        :is-open="isModalOpen"
-        :contributor-list="modalContent"
-        @close="isModalOpen = false"
-      />
+    <CubeLoader v-if="isPending" />
 
-      <CubeLoader v-if="isPending" />
+    <div
+      v-else-if="isError"
+      class="fixed inset-0 flex items-center justify-center bg-white p-1.5"
+    >
+      An error occurred while fetching data. Please try again later.
+    </div>
 
-      <div
-        v-else-if="isError"
-        class="fixed inset-0 flex items-center justify-center bg-white p-1.5"
-      >
-        An error occurred while fetching data. Please try again later.
-      </div>
-
-      <StudentList
-        v-else-if="data"
-        :student-list="filteredStudentList"
-        @open-contributors-modal="handleOpenContributorsModal"
-      />
-    </main>
-  </div>
+    <StudentList
+      v-else-if="data"
+      :student-list="filteredStudentList"
+      @open-contributors-modal="handleOpenContributorsModal"
+    />
+  </main>
 </template>
